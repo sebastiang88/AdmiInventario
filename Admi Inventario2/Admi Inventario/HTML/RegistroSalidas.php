5543acd30e17e6ef34e1c2ba8e9b1c8c90033061
sebastiang88 <sebastianguevaracruz@gmail.com>
2025-05-22 09:09:59 -0500

    Actualizo agregar salidas HU04

--- Admi Inventario2/Admi Inventario/HTML/RegistroSalidas.php
+++ Admi Inventario2/Admi Inventario/HTML/RegistroSalidas.php
@@ -1,4 +1,5 @@
 <?php include("../conexiones/db.php")  ?>
+
 <?php
 
 session_start();
@@ -38,7 +39,31 @@ die();
       </tr>
     </thead>
     <tbody>
+    <?php
+      $query = "SELECT * FROM salidas";
+      $result_salidas = mysqli_query($conn,$query);
 
+      while ($row = mysqli_fetch_array($result_salidas)) { ?>
+        <tr>
+          <td align="center"><?php echo $row['IdSalidas'] ?></td>
+          <td align="center"><?php echo $row['FacturaSalidas'] ?></td>
+          <td align="center"><?php echo $row['FechaSalidas'] ?></td>
+          <td align="center"><?php echo $row['HoraSalidas'] ?></td>
+          <td align="center"><?php echo $row['Estado'] ?></td>
+          <td align="center"><?php echo $row['IdCliente'] ?></td>
+          <td>
+           <a href="../conexiones/edit_salida.php?IdSalidas=<?php echo $row['IdSalidas']?>">
+            <?php  if ($arreglo1[0] ==1) {
+             echo"<img src='../imagenes/lapiz.jpg' width='25px' height='25px'>";
+             } ?>           
+           </a>
+           <a href="../conexiones/delete_task_salida.php?IdSalidas=<?php echo $row['IdSalidas']?>">
+            <?php  if ($arreglo1[0] ==1) {
+             echo"<img src='../imagenes/basura.png' width='25px' height='25px'>";
+              } ?>
+           </a> 
+          </td>
+    <?php } ?>
     </tbody>
 </table>
 </center>
