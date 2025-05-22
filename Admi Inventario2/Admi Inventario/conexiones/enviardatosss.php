34cda77d28622cb573d1bdd05251e09061805c3a
sebastiang88 <sebastianguevaracruz@gmail.com>
2025-05-21 20:43:41 -0500

    Actualizo archivo agregar entradas HU05

--- Admi Inventario2/Admi Inventario/conexiones/enviardatosss.php
+++ Admi Inventario2/Admi Inventario/conexiones/enviardatosss.php
@@ -1,3 +1,22 @@
 <?php
 include("db.php");
 
+if (isset($_POST['Agregar'])) {
+  $id_Entrada= $_POST['id_Entrada'];
+  $FacturaEntrada= $_POST['FacturaEntrada'];
+  $FechaEntrada= $_POST['FechaEntrada'];
+  $HoraEntrada= $_POST['HoraEntrada'];
+
+  
+ $query="INSERT INTO entradas(id_Entrada,FacturaEntrada,FechaEntrada,HoraEntrada) VALUES ('$id_Entrada','$FacturaEntrada','$FechaEntrada','$HoraEntrada')";
+  
+  
+  $result=mysqli_query($conn,$query);
+
+  if (!$result) {
+  	die("query failed");
+  }
+
+  header("Location:../HTML/AgregarEntrada.php");
+}
+?>
